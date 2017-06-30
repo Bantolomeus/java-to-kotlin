4100603c7e31c389077d95178c8939c5e9bfa364
chrb <chrb@spreadshirt.net>
2017-06-30 09:23:13 +0200

    first commit

---
+++ src/main/java/com/bantolomeus/JavaToKotlinApplication.java
@@ -0,0 +1,12 @@
+package com.bantolomeus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaToKotlinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaToKotlinApplication.class, args);
+	}
+}
---
+++ src/main/java/com/bantolomeus/model/DistributionCreatorModel.java
@@ -0,0 +1,5 @@
+package com.bantolomeus.model;
+
+public class DistributionCreatorModel {
+
+}
